3583340a2f491c7d4ba93316e5a62a39fc3f0de4
bichnq <bichnq@deha-soft.com>
2025-05-24 21:43:24 +0700

    feat: Initialize HistoryMadeSimple Member frontend with Vite and React

---
+++ FrontEnd/HistoryMadeSimple/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // file index.html gốc
+    "./src/**/*.{js,jsx,ts,tsx}" // tất cả file JS/TS trong src (react)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
